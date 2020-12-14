cbef709d2f78f560a9a9d0da30508a74bd9a53ad
birthg <75948076+birthg@users.noreply.github.com>
2020-12-14 12:57:32 +0530

    Add sus files

---
+++ main.js
@@ -0,0 +1,12 @@
+
+!--hide
+var password;
+var pass="12345";
+password=prompt('enter password to view webpage','');
+if(password==pass)
+alert('correct password,click ok to enter website.');
+else
+ {
+  window.location="birth.html";
+  }
+  //-->
